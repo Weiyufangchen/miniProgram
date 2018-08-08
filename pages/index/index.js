a0726931b9dcd92fe201d200f454f24aa3a0b96e
zhouzhichao <342412187@qq.com>
2018-08-08 15:51:59 +0800

    获取用户信息，swiper轮播组件

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,34 +5,81 @@ Page({
    * 页面的初始数据
    */
   data: {
-    msg: '小程序'
-  },
-  clickParent () {
-    console.log('点击父元素')
-  },
-  clickChild () {
-    console.log('点击子元素')
+    msg: '夏雨',
+    userInfo: {},
+    isShow: true // true表示没有授权，显示按钮
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('onLoad 页面加载')
+    console.log(this)
+    this.getUserInfo()
   },
 
+  getUserInfo () {
+    // 判断用户是否授权
+    wx.getSetting({
+      success: (data) => {
+        console.log(data)
+        if (data.authSetting['scope.userInfo']) {
+          // 用户已经授权
+          this.setData({
+            isShow: false
+          })
+        } else {
+          // 没有授权
+          this.setData({
+            isShow: true
+          })
+        }
+      },
+    })
+    // 获取用户登录的信息
+    wx.getUserInfo({
+      success: data => {
+        console.log(data)
+        // 更新data中userInfo
+        this.setData({
+          userInfo: data.userInfo
+        })
+      },
+      fail: () => {
+        console.log('获取用户数据失败')
+      }
+    })
+  },
+
+  // 点击按钮处理用户数据
+  handleUserInfo (data) {
+    console.log('用户点击了', data)
+    // 判断用户点击的是否是允许
+    if (data.detail.rawData) {
+      // 用户点击的是允许
+      this.getUserInfo();
+    }
+  },
+
+  // 点击跳转list界面
+  handleClick () {
+    wx.navigateTo({
+      url: '/pages/list/list',
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    console.log('onReady 页面初次渲染完成')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    console.log('onShow 页面显示')
   },
 
   /**
